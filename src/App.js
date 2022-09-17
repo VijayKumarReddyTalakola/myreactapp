29da5039c126c0998355f862442cfa6b3cb41e36
VijayKumarReddyTalakola <109469574+VijayKumarReddyTalakola@users.noreply.github.com>
2022-09-17 10:33:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import Cricscore from "./Components/Cricscore";
+export default function App() {
+  return (
+    <div className="App">
+      <Cricscore />
+    </div>
+  );
+}
